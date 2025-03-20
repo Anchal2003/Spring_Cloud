b6100107e03e87d4db8ef40b8c89c64476988d13
Anchal2003 <sahuanchal22@gmail.com>
2025-03-20 23:26:27 +0530

    [Anchal Sahu] Add application properties  to conneting the MYSQL database.

---
+++ jdbcDemo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien(id,name,tech) values (101,'manoj','AI');
+insert into alien(id,name,tech) values (102,'Karan','java');
+insert into alien(id,name,tech) values (103,'Anuj','Python');
